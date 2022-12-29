84163954c235c9675fd334c5a3a77e256bdc8384
sajjad arghaee <84894722+Sajjad-Arghaee@users.noreply.github.com>
2022-12-29 12:51:42 +0330

    adding new commands

--- compile.sh
+++ compile.sh
@@ -2,4 +2,7 @@ gcc -o ls ls.c
 gcc -o pwd pwd.c
 gcc -o cd cd.c
 gcc -o main main.c -lreadline
+gcc -o first_word first_word.c
+gcc -o remove_empty_spaces remove_empty_spaces.c
+gcc -o most_frequency most_frequency.c
 ./main
