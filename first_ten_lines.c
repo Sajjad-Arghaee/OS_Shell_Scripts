78e13a422a561df944f978d70ebe99d89d7e18ac
YosefRezazadeh <102348673+YosefRezazadeh@users.noreply.github.com>
2022-12-30 13:01:58 +0000

    first_ten_lines modified to support absolute path

--- first_ten_lines.c
+++ first_ten_lines.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int main (int argc, char *argv[])
 {
-	FILE *file = fopen(argv[0],"r");
+	FILE *file;
+    if(access(argv[0], F_OK)==0){
+        file = fopen(argv[0], "r") ;
+    }else{
+        if(access(argv[1], F_OK)==0){
+            file = fopen(argv[1], "r");
+        }else{
+            fprintf(stderr, "No such file\n");
+        }
+    }
     int bufferLength = 255;
     char buffer[bufferLength];
     int count = 1;
