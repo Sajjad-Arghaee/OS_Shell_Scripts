632c284bfaf1fa1c5ba63bf5fba81b6dc3100c90
YosefRezazadeh <102348673+YosefRezazadeh@users.noreply.github.com>
2022-12-30 14:59:48 +0000

    first_word modified to support absolute path

--- first_word.c
+++ first_word.c
@@ -1,11 +1,21 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <unistd.h> 
 
 int main(int argc, char *argv[]) {
-    FILE *file = fopen(argv[0], "r");
+    FILE *file;
+    if(access(argv[0], F_OK)==0){
+        file = fopen(argv[0], "r") ;
+    }else{
+        if(access(argv[1], F_OK)==0){
+            file = fopen(argv[1], "r");
+        }else{
+            fprintf(stderr, "No such file\n");
+        }
+    }
     char* word;
     if ( file == NULL )
     {
-        printf( "Intended file failed to open" ) ;
+        // printf( "Intended file failed to open" ) ;
     }
     else{
         fscanf(file,"%s%*[^\n]",word);
@@ -14,3 +24,4 @@ int main(int argc, char *argv[]) {
     fclose(file);
     return(0);
 }
+
